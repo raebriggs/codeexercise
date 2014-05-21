7871637d4ffecb53d909b4b1b521663efdb06dd0
Rae Briggs <rae@Raes-Mac-Pro.local>
2014-05-21 00:31:29 -0600

    first run shuffle works

--- CodeExercise/CodeExerciseViewController.h
+++ CodeExercise/CodeExerciseViewController.h
@@ -9,5 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface CodeExerciseViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *outputLabel;
+@property (weak, nonatomic) IBOutlet UIButton *shuffleButton;
+@property (weak, nonatomic) IBOutlet UITextField *countInputTextField;
+- (IBAction)didTapShuffle:(id)sender;
 
 @end
--- CodeExercise/CodeExerciseViewController.m
+++ CodeExercise/CodeExerciseViewController.m
@@ -14,6 +14,8 @@
 
 @implementation CodeExerciseViewController
 
+@synthesize countInputTextField,outputLabel;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -26,4 +28,46 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)didTapShuffle:(id)sender {
+    if(countInputTextField.text.length<=0){
+        //show an error
+    }else{
+        NSString *count = countInputTextField.text;
+        
+        [self shuffleIt: [count intValue] ];
+    }
+}
+/**
+ *Use the Knuth-Fisher-Yates algorithm
+ */
+ 
+-(void)shuffleIt:(int) count
+{
+    NSMutableArray *array = [self generateOrderedArray:count];
+    outputLabel.text = [[self knuth_fisher_yates:array] componentsJoinedByString:@","];
+    //[array componentsJoinedByString:@","];
+    
+}
+-(NSMutableArray*)knuth_fisher_yates:(NSMutableArray*)array
+{
+    for(int i=array.count-1;i>=0;i--){
+        //generate random
+        int random = arc4random_uniform(i);
+        //temp store obj (maybe its not a number!)
+        id temp = [array objectAtIndex:random];
+        //swap em
+        array[random] = array[i];
+        array[i] = temp;
+    }
+    return array;
+}
+-(NSMutableArray*)generateOrderedArray:(int)count
+{
+    NSMutableArray *array = [[NSMutableArray alloc] init];
+    for(int i=0;i<count;i++)
+    {
+        array[i]=[NSNumber numberWithInt:i+1];
+    }
+    return array;
+}
 @end
